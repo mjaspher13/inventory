8bf3f49007d26bd040c8bb56ad84cd5d5235bff2
jaspher <jaspher@esixtycorporation.com>
2020-04-15 23:23:50 +0800

    * forgot to add /

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const { appUrl } = require("../config/config");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { appurl: appUrl });
+  res.render("index", { appurl: appUrl + "/" });
 });
 
 module.exports = router;
