d5ffe593cd5c4431e79717475e4346be7ded90a2
jaspher <jaspher@esixtycorporation.com>
2020-04-15 23:19:41 +0800

    * fixed app url for heroku deploy

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var router = express.Router();
+const dotenv = require("dotenv");
+const { appUrl } = require("../config/config");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  let http = req.protocol + "://";
-  let url = http + req.headers.host + "/";
-  res.render("index", { appurl: url });
+  res.render("index", { appurl: appUrl });
 });
 
 module.exports = router;
